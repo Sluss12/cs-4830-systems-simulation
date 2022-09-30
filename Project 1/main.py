8b506bc4678de8b1e858a88e489aba50bc26f474
Matthew Slusser <slusser12@gmail.com>
2022-09-30 14:21:01 -0400

    i should've gotten more done.....\

--- Project 1/main.py
+++ Project 1/main.py
@@ -5,6 +5,7 @@
 @project: 1
 @description: For this project we are simulating two different drive-thru scenarios. This drive-thru simulation consists of a pair of order stations and payment/pickup window, as space is limited our goal is to optimize throughput of cars to maximize profit during the lunch rush that occurs from 11am - 1pm, or over a 2 hour window.
 """
+#%% cell to view graphs
 
 # list of resources
 import simpy
@@ -30,3 +31,5 @@ def customerGenerator(env, cashier):
         env.process(cust.getInLine())
         custNo += 1
         yield env.timeout(random.expovariate(1/6.0))
+
+# Watching Lecture on 9/2/22 at timestamp 33:29
